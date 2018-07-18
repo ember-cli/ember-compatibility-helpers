17eafdb977959cef3f326f652d9c6477c1dfcb5e
Stefan Penner <stefan.penner@gmail.com>
2018-07-18 14:26:06 -0700

    Make work with parallel builds. Also fixes duck-typing issue for project mock.
    More details:
    
      * https://github.com/babel/broccoli-babel-transpiler#parallel-transpilation

--- index.js
+++ index.js
@@ -1,54 +1,10 @@
 'use strict';
 
 const VersionChecker = require('ember-cli-version-checker');
+
 const semver = require('semver');
 const satisfies = semver.satisfies;
 const gte = semver.gte;
-
-function comparisonPlugin(babel) {
-  const t = babel.types;
-
-  const trueIdentifier = t.identifier('true');
-  const falseIdentifier = t.identifier('false');
-
-  return {
-    name: "ember-compatibility-helpers",
-    visitor: {
-      ImportSpecifier(path, state) {
-        if (path.parent.source.value === 'ember-compatibility-helpers') {
-          let importedName = path.node.imported.name;
-          if (importedName === 'gte') {
-            state.gteImportId = state.gteImportId || path.scope.generateUidIdentifierBasedOnNode(path.node.id);
-            path.scope.rename(path.node.local.name, state.gteImportId.name);
-            path.remove();
-          }
-
-          if (importedName === 'lte') {
-            state.lteImportId = state.lteImportId || path.scope.generateUidIdentifierBasedOnNode(path.node.id);
-            path.scope.rename(path.node.local.name, state.lteImportId.name);
-            path.remove();
-          }
-        }
-      },
-
-      CallExpression(path, state) {
-        if (state.gteImportId && path.node.callee.name === state.gteImportId.name) {
-          let argument = path.node.arguments[0];
-          let replacementIdentifier = semver.gte(state.opts.emberVersion, argument.value) ? trueIdentifier : falseIdentifier;
-
-          path.replaceWith(replacementIdentifier);
-        } else if (state.lteImportId && path.node.callee.name === state.lteImportId.name) {
-          let argument = path.node.arguments[0];
-          let replacementIdentifier = semver.lte(state.opts.emberVersion, argument.value) ? trueIdentifier : falseIdentifier;
-
-          path.replaceWith(replacementIdentifier);
-        }
-      }
-    }
-  };
-}
-
-comparisonPlugin.baseDir = () => __dirname;
 
 module.exports = {
   name: 'ember-compatibility-helpers',
@@ -102,7 +58,7 @@ module.exports = {
   _getComparisonPlugin() {
     const trueEmberVersion = this.emberVersion.match(/\d+\.\d+\.\d+/)[0];
 
-    return [comparisonPlugin, { emberVersion: trueEmberVersion }];
+    return [require.resolve('./comparision-plugin.js'), { emberVersion: trueEmberVersion }];
   },
 
   _getDebugPlugin(emberVersion, parentChecker) {
