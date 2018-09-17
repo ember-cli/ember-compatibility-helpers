dd8517e7ac6e3ba913b507b62dbee98db41088d5
Alex LaFroscia <alex@lafroscia.com>
2018-09-17 15:42:17 -0700

    chore: upgrade `babel-plugin-debug-macros`

--- index.js
+++ index.js
@@ -64,43 +64,34 @@ module.exports = {
   _getDebugPlugin(emberVersion, parentChecker) {
     const trueEmberVersion = emberVersion.match(/\d+\.\d+\.\d+/)[0];
 
-    let options = {
-      envFlags: {
-        source: 'ember-compatibility-helpers',
-        flags: {
-          DEBUG: false
-        }
-      },
-
-      features: {
-        name: 'ember-compatibility-helpers',
-        source: 'ember-compatibility-helpers',
-        flags: {
-          HAS_UNDERSCORE_ACTIONS: !gte(trueEmberVersion, '2.0.0'),
-          HAS_MODERN_FACTORY_INJECTIONS: gte(trueEmberVersion, '2.13.0'),
-          HAS_DESCRIPTOR_TRAP: satisfies(trueEmberVersion, '~3.0.0'),
-          HAS_NATIVE_COMPUTED_GETTERS: gte(trueEmberVersion, '3.1.0-beta.1'),
-
-          IS_GLIMMER_2: gte(trueEmberVersion, '2.10.0'),
-
-          SUPPORTS_FACTORY_FOR: gte(trueEmberVersion, '2.12.0') || parentChecker.for('ember-factory-for-polyfill', 'npm').gte('1.0.0'),
-          SUPPORTS_GET_OWNER: gte(trueEmberVersion, '2.3.0') || parentChecker.for('ember-getowner-polyfill', 'npm').gte('1.1.0'),
-          SUPPORTS_SET_OWNER: gte(trueEmberVersion, '2.3.0'),
-          SUPPORTS_NEW_COMPUTED: gte(trueEmberVersion, '1.12.0-beta.1'),
-          SUPPORTS_INVERSE_BLOCK: gte(trueEmberVersion, '1.13.0'),
-          SUPPORTS_CLOSURE_ACTIONS: gte(trueEmberVersion, '1.13.0'),
-          SUPPORTS_UNIQ_BY_COMPUTED: gte(trueEmberVersion, '2.7.0')
-        }
-      },
-
-      externalizeHelpers: {
-        global: 'Ember'
-      },
-
+    const options = {
       debugTools: {
-        source: 'ember-compatibility-helpers',
-        assertPredicateIndex: 1
-      }
+        isDebug: process.env.EMBER_ENV !== 'production',
+        source: 'ember-compatibility-helpers'
+      },
+
+      flags: [
+        {
+          name: 'ember-compatibility-helpers',
+          source: 'ember-compatibility-helpers',
+          flags: {
+            HAS_UNDERSCORE_ACTIONS: !gte(trueEmberVersion, '2.0.0'),
+            HAS_MODERN_FACTORY_INJECTIONS: gte(trueEmberVersion, '2.13.0'),
+            HAS_DESCRIPTOR_TRAP: satisfies(trueEmberVersion, '~3.0.0'),
+            HAS_NATIVE_COMPUTED_GETTERS: gte(trueEmberVersion, '3.1.0-beta.1'),
+
+            IS_GLIMMER_2: gte(trueEmberVersion, '2.10.0'),
+
+            SUPPORTS_FACTORY_FOR: gte(trueEmberVersion, '2.12.0') || parentChecker.for('ember-factory-for-polyfill', 'npm').gte('1.0.0'),
+            SUPPORTS_GET_OWNER: gte(trueEmberVersion, '2.3.0') || parentChecker.for('ember-getowner-polyfill', 'npm').gte('1.1.0'),
+            SUPPORTS_SET_OWNER: gte(trueEmberVersion, '2.3.0'),
+            SUPPORTS_NEW_COMPUTED: gte(trueEmberVersion, '1.12.0-beta.1'),
+            SUPPORTS_INVERSE_BLOCK: gte(trueEmberVersion, '1.13.0'),
+            SUPPORTS_CLOSURE_ACTIONS: gte(trueEmberVersion, '1.13.0'),
+            SUPPORTS_UNIQ_BY_COMPUTED: gte(trueEmberVersion, '2.7.0')
+          }
+        }
+      ]
     };
 
     return [require.resolve('babel-plugin-debug-macros'), options];
