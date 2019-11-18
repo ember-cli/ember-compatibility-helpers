a949dedd0aa37bb0162d4f413f25153f3d6741ca
Francesco Novy <francesco@cropster.com>
2019-11-18 11:50:54 +0100

    Update type definitions for lte/gte

--- build/types.js
+++ build/types.js
@@ -8,7 +8,9 @@ const booleanFlags = require('./boolean-flags');
  */
 module.exports = [
   'export function gte(version: string): boolean;',
-  'export function lte(version: string): boolean;'
+  'export function gte(package: string, version: string): boolean;',
+  'export function lte(version: string): boolean;',
+  'export function lte(package: string, version: string): boolean;'
 ]
   .concat(booleanFlags.map(b => `export const ${b}: boolean;`))
   .join('\n');
